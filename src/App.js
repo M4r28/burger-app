1dc5447cec4238a3a2f4618a9467dbc969d6c67b
Marta <marta.marta.k@gmail.com>
2020-06-14 15:24:41 +0200

    switchNameHandler passed as a prop to a child component

--- src/App.js
+++ src/App.js
@@ -28,9 +28,16 @@ this.setState({persons: [
        <h1>Hi, I'm react App</h1>
        <p>This is really working!</p>
        <button onClick={this.switchNameHandler}>Switch Name</button>
-       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>And I love singing!</Person>
-       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobby is writing.</Person>
-       <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+       <Person 
+       name={this.state.persons[0].name} 
+       age={this.state.persons[0].age}>And I love singing!</Person>
+       <Person 
+       name={this.state.persons[1].name} 
+       age={this.state.persons[1].age}
+       click={this.switchNameHandler}>My hobby is writing.</Person>
+       <Person 
+       name={this.state.persons[2].name} 
+       age={this.state.persons[2].age} />
       </div>
     );
   }
