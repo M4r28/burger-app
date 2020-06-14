b1fe74ea22423322f6ca0d12a231dc5e1f102afc
Marta <marta.marta.k@gmail.com>
2020-06-14 15:46:27 +0200

    passing an argument with bind and arror function

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ class App extends Component {
     ]
   }
 
-switchNameHandler =()=>{
+switchNameHandler =(newName)=>{
 // console.log('was clicked');
 this.setState({persons: [
-  {name:'Lola', age: 36},
+  {name: newName, age: 36},
   {name:'Joy', age: 40},
   {name:'Cassandra', age: 59}
 
@@ -27,14 +27,14 @@ this.setState({persons: [
       <div className="App">
        <h1>Hi, I'm react App</h1>
        <p>This is really working!</p>
-       <button onClick={this.switchNameHandler}>Switch Name</button>
+       <button onClick={()=>this.switchNameHandler('Austin')}>Switch Name</button>
        <Person 
        name={this.state.persons[0].name} 
        age={this.state.persons[0].age}>And I love singing!</Person>
        <Person 
        name={this.state.persons[1].name} 
        age={this.state.persons[1].age}
-       click={this.switchNameHandler}>My hobby is writing.</Person>
+       click={this.switchNameHandler.bind(this, 'Lola')}>My hobby is writing.</Person>
        <Person 
        name={this.state.persons[2].name} 
        age={this.state.persons[2].age} />
